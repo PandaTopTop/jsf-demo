c6469ac3e24c47841ae47226b6a7f29e8090ccb6
Volodymyr Dzhalilov <vladimirdzahalilov@gmail.com>
2026-03-12 13:50:18 +0200

    Initial commit

---
+++ src/main/java/HelloWorldBean.java
@@ -0,0 +1,13 @@
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
+
+import java.io.Serializable;
+
+@Named("helloworld")
+@ViewScoped
+public class HelloWorldBean implements Serializable {
+
+    public String getMessage(){
+        return "Hello World from Planet!";
+    }
+}
